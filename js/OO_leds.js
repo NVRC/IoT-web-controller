9ecbef0e2c84c9cdcbaea3b04c8b5917c0708f65
NVRC <nathaniel.charlebois@gmail.com>
2018-01-22 21:21:55 -0500

    Added basic two color gradient selection

--- js/OO_leds.js
+++ js/OO_leds.js
@@ -9,7 +9,8 @@
 const NUM_LEDS = 60;
 var ledArray = new Array(NUM_LEDS);
 var colorSel = document.getElementById('colorSel');
-var previous = -1;
+const INIT = -1;
+var previous = INIT;
 
 class ledButton {
     // The constructor encompasses function declarations
@@ -26,6 +27,10 @@ class ledButton {
 
         this.isActive = function(){
             return _active;
+        }
+
+        this.getColor = function(){
+            return _color;
         }
 
         this.setActive = function(){
@@ -48,24 +53,25 @@ class ledButton {
             temp[_id].style.background = _color;
 
             _active = true;
-
-            // Set the global previous led touched by the color selector
-            previous = _id;
-            //ledArray[_id].findPair();
         }
+
 
         this.findPair = function(){
             for(i = 0; i < NUM_LEDS; i++){
-                var temp = ledArray[i];
-                console.log(temp.isActive());
-                if(temp.isActive()){
-                    console.log('temp '+temp.getPair());
-                    temp.setPair(12);
-                    console.log('temp '+ temp.getPair());
-                    temp.setPair(false);
+                    if(i != _id){
+                    var temp = ledArray[i];
+                    if(temp.isActive()){
+                        if(temp.getPair() > INIT){
+                            console.log(i + ' should pair with '+ _id);
 
-                    if(!temp.getPair()){
-                        console.log(_id+' is not paired.');
+                            temp.setPair(_id);
+                            this.setPair(i);
+                            // Draw a gradient across the leds that exist
+                            // between the pair
+                            drawLedGradient( _id, i );
+                        } else {
+                            console.log(i+' is not paired.');
+                        }
                     }
                 }
             }
@@ -78,24 +84,45 @@ class ledButton {
         }
 
         this.changeColor = function(){
-            console.log('changeColor() this.getId()'+this);
-            console.log('changeColor() id: '+_id);
+            // Set the global previous led touched by the color selector
+            previous = _id;
             ledArray[_id].setColor(this.value);
+            ledArray[_id].findPair();
         }
 
         this.isClicked = function(){
             // Unbind the previous led touched by the color selector
-            if(previous > -1){
+            if(previous > INIT){
                 colorSel.removeEventListener('change',ledArray[previous].changeColor);
             }
 
             // Initiate the color selector popup and color selection sequence
             colorSel.click();
-            console.log('isClicked() id: '+_id);
             colorSel.addEventListener('change',ledArray[_id].changeColor);
         }
     }
 };
+
+function drawLedGradient( anchor, found ){
+    let offset = anchor - found;
+    let grad = new GradientArray();
+    let steps = Math.abs(offset)+1;
+    var stepGradient;
+    if(offset > 0){
+        stepGradient = grad.generateGradient(ledArray[found].getColor()
+            ,ledArray[anchor].getColor()
+            ,steps);
+    } else {
+        stepGradient = grad.generateGradient(ledArray[anchor].getColor()
+            ,ledArray[found].getColor()
+            ,steps);
+    }
+    var gradientHelper = 0;
+    for(i = found+1; i < anchor;i++){
+        ledArray[i].setColor(stepGradient[gradientHelper]);
+        gradientHelper++;
+    }
+}
 
 function buildLeds(){
     var container = document.getElementById('led_buttons');
