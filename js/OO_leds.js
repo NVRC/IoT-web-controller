ed1aba4802c56920a030529f526092bc04a2e1a0
NVRC <nathaniel.charlebois@gmail.com>
2018-01-22 22:54:01 -0500

    Added support for 3rd order linear gradients. N level to come

--- js/OO_leds.js
+++ js/OO_leds.js
@@ -11,6 +11,7 @@ var ledArray = new Array(NUM_LEDS);
 var colorSel = document.getElementById('colorSel');
 const INIT = -1;
 var previous = INIT;
+const DEFAULT_COLOR = "#ffffff";
 
 class ledButton {
     // The constructor encompasses function declarations
@@ -18,12 +19,11 @@ class ledButton {
     // This structure provides private object properties
     // where private data is captured in closures and thereby
     // inaccessible in other scopes.
-    constructor( id, self ){
+    constructor( id ){
             let _id = id;
             let _active = false;
             let _pair = false;
             let _color = false;
-            let _self = self;
 
         this.isActive = function(){
             return _active;
@@ -38,11 +38,23 @@ class ledButton {
         }
 
         this.getPair = function(){
+            
             return _pair;
         }
 
         this.setPair = function( id ){
             _pair = id;
+        }
+
+        this.reset = function(){
+            // Set the DOM button color
+            var temp = document.querySelectorAll( '.round_button' );
+            temp[_id].style.background = DEFAULT_COLOR;
+
+            _active = false;
+            _color = false;
+            _pair = false;
+
         }
 
         this.setColor = function( value ){
@@ -54,30 +66,6 @@ class ledButton {
 
             _active = true;
         }
-
-
-        this.findPair = function(){
-            for(i = 0; i < NUM_LEDS; i++){
-                    if(i != _id){
-                    var temp = ledArray[i];
-                    if(temp.isActive()){
-                        if(temp.getPair() > INIT){
-                            console.log(i + ' should pair with '+ _id);
-
-                            temp.setPair(_id);
-                            this.setPair(i);
-                            // Draw a gradient across the leds that exist
-                            // between the pair
-                            drawLedGradient( _id, i );
-                        } else {
-                            console.log(i+' is not paired.');
-                        }
-                    }
-                }
-            }
-        }
-
-
 
         this.getId = function(){
             return _id;
@@ -100,40 +88,91 @@ class ledButton {
             colorSel.click();
             colorSel.addEventListener('change',ledArray[_id].changeColor);
         }
+
+
+        this.findPair = function(){
+            for(let i = 0; i < NUM_LEDS; i++){
+                    if(i != _id){
+                    var temp = ledArray[i];
+                    if(temp.isActive()){
+                        console.log(temp.getPair());
+                        if(!temp.getPair()){
+                            console.log(i + ' should pair with '+ _id);
+
+                            temp.setPair(_id);
+                            this.setPair(i);
+                            // Draw a gradient across the leds that exist
+                            // between the pair
+                            drawLedGradient( _id, i );
+                        } else {
+                            // Paired use case
+                            // Determine if
+                            //      pair --- sel --- pair
+                            //  or
+                            //      sel --- pair --- pair
+                            let pair = temp.getPair();
+                            console.log('anchor: '+_id+'\t target: '+pair);
+                            console.log('anchor: '+_id+'\t target: '+i);
+                            drawLedGradient( _id, pair );
+                            drawLedGradient( _id, i );
+                        }
+                    } else {
+                        // Do nothing if the led is still in default state
+                    }
+                }
+            }
+        }
     }
 };
 
-function drawLedGradient( anchor, found ){
-    let offset = anchor - found;
+function drawLedGradient( anchor, target ){
+    console.log('DRAW anchor: '+anchor+'\t target: '+target);
+    let offset = anchor - target;
+    console.log('offset: '+offset);
     let grad = new GradientArray();
     let steps = Math.abs(offset)+1;
     var stepGradient;
     if(offset > 0){
-        stepGradient = grad.generateGradient(ledArray[found].getColor()
+        stepGradient = grad.generateGradient(ledArray[target].getColor()
             ,ledArray[anchor].getColor()
             ,steps);
     } else {
         stepGradient = grad.generateGradient(ledArray[anchor].getColor()
-            ,ledArray[found].getColor()
+            ,ledArray[target].getColor()
             ,steps);
     }
     var gradientHelper = 0;
-    for(i = found+1; i < anchor;i++){
+    var swapAnchor = anchor;
+    var swapTarget = target;
+    if(offset < 0){
+        swapAnchor = target;
+        swapTarget = anchor;
+    }
+    for(let i = swapTarget+1; i < swapAnchor;i++){
         ledArray[i].setColor(stepGradient[gradientHelper]);
+        ledArray[i].setPair(true);
         gradientHelper++;
+
+
     }
 }
 
 function buildLeds(){
     var container = document.getElementById('led_buttons');
-    for(i=0;i<NUM_LEDS;i++){
+    for(let i=0;i<NUM_LEDS;i++){
 
         var div = document.createElement('div');
-        ledArray[i] = new ledButton( i, div );
+        ledArray[i] = new ledButton( i );
         div.className = 'round_button';
 
         div.addEventListener("click",ledArray[i].isClicked);
         container.appendChild(div);
+    }
+}
+
+function reset(){
+    for(let i=0;i<NUM_LEDS;i++){
+        ledArray[i].reset();
     }
 }
 
