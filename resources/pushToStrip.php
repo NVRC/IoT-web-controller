5b0e03dcde65cb34eb9339c20c9d6e60f9bc37e2
NVRC <nathaniel.charlebois@gmail.com>
2018-08-29 20:14:38 -0400

    shell_exec() rides again. hopefully patches the sys.argv python error

--- resources/pushToStrip.php
+++ resources/pushToStrip.php
@@ -8,9 +8,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     for ($x = 0; $x < 60; $x++){
         $colorArray.$_POST[strval($x)];
     }
-    exec("python3 .$config['paths']['cgi'] .$colorArray .$_POST['brightness']");
+    //exec("python3 .$config['paths']['cgi'] .$colorArray .$_POST['brightness']");
 
-    // shell_exec('python3 '.$config['paths']['cgi'].$colorArray.' '.$_POST['brightness']);
+    shell_exec('python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$_POST['brightness']);
 } else {
     //  GET
     echo 'Whatcha tryna pull here? Huh?';
@@ -18,9 +18,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     for ($x = 0; $x < 60; $x++){
         $colorArray.$_GET[strval($x)];
     }
-    exec("python3 .$config['paths']['cgi'] .$colorArray .$_GET['brightness']");
+    //exec("python3 .$config['paths']['cgi'] .$colorArray .$_GET['brightness']");
 
-    //shell_exec('python3 '.$config['paths']['cgi'].$colorArray.' '.$_POST['brightness']);
+    shell_exec('python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$_GET['brightness']);
 }
 
 ?>
