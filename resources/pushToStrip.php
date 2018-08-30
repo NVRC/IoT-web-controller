ec22510830d6f5c9767f41a1a18f84716595fba9
NVRC <nathaniel.charlebois@gmail.com>
2018-08-29 20:57:35 -0400

    Adding brightness by reference

--- resources/pushToStrip.php
+++ resources/pushToStrip.php
@@ -2,11 +2,12 @@
 require_once("config.php");
 
 $colorArray = '';
+$brightness;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_POST as $key => $value){
-        handlePair($key,$value,$colorArray);
+        handlePair($key,$value,$colorArray,$brightness);
     }
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness;
     shell_exec($execString);
@@ -15,17 +16,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_GET as $key => $value){
-        handlePair($key,$value,$colorArray);
+        handlePair($key,$value,$colorArray,$brightness);
     }
 
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness;
     shell_exec($execString);
 }
-function handlePair($k,$v,&$src){
+function handlePair($k,$v,&$src,&$btr){
     if($k == 'brightness'){
-        $brightness = $v;
+        $btr = $v;
     } else {
         $src.=$v;
+
     }
 }
 
