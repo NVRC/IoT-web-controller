0515f9d30e003e694981517e730ac14c98d5bf72
NVRC <nathaniel.charlebois@gmail.com>
2018-08-29 20:50:13 -0400

    Code refactor/cleanup

--- resources/pushToStrip.php
+++ resources/pushToStrip.php
@@ -5,30 +5,28 @@ $colorArray = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Handle AJAX cgi bin use case (COMMON)
-    for ($x = 0; $x < 60; $x++){
-        $colorArray.$_POST[strval($x)];
+    foreach($_POST as $key => $value){
+        handlePair($key,$value);
     }
-    //exec("python3 .$config['paths']['cgi'] .$colorArray .$_POST['brightness']");
-
-    echo shell_exec('python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$_POST['brightness']);
+    $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness;
+    shell_exec($execString);
 } else {
     //  GET
-    $colorArray = '';
+
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_GET as $key => $value){
-        echo $key.' : '.$value;
-        if($key == 'brightness'){
-            $brightness = $value;
-        } else {
-            $colorArray.=$value;
-        }
+        handlePair($key,$value);
     }
-    //exec("python3 .$config['paths']['cgi'] .$colorArray .$_GET['brightness']");
-    echo $colorArray;
-    echo $_GET['brightness'];
+
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness;
-    echo $execString;
-    echo shell_exec($execString);
+    shell_exec($execString);
+}
+function handlePair($k,$v){
+    if($k == 'brightness'){
+        $brightness = $v;
+    } else {
+        $colorArray.=$v;
+    }
 }
 
 ?>
