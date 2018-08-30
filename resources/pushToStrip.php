8948aa8b219ec8a0b63f418e52dd96f5647bad23
NVRC <nathaniel.charlebois@gmail.com>
2018-08-29 20:52:31 -0400

    Passing str by reference to handlePair function

--- resources/pushToStrip.php
+++ resources/pushToStrip.php
@@ -6,7 +6,7 @@ $colorArray = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_POST as $key => $value){
-        handlePair($key,$value);
+        handlePair($key,$value,$colorArray);
     }
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness;
     shell_exec($execString);
@@ -15,17 +15,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_GET as $key => $value){
-        handlePair($key,$value);
+        handlePair($key,$value,$colorArray);
     }
 
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness;
     shell_exec($execString);
 }
-function handlePair($k,$v){
+function handlePair($k,$v,&$src){
     if($k == 'brightness'){
         $brightness = $v;
     } else {
-        $colorArray.=$v;
+        $src.=$v;
     }
 }
 
