b730ce4d9e2c3d0d595c833199a63a9b5b1e35ca
NVRC <nathaniel.charlebois@gmail.com>
2018-08-29 22:53:29 -0400

    Added support for mobile, desktop and tablet

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -22,7 +22,7 @@
 
     <div class="container-fluid">
     	<div class="row">
-    		<div class="col-md-12">
+    		<div class="col-md-12 col-sm-12 col-lg-12">
     			<div class="page-header">
     				<h1>
     					IoT <small>web controller</small>
