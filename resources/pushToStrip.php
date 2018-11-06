5866e4a7084b567d8da9b8bd8fad492c010b9143
NVRC <nathaniel.charlebois@gmail.com>
2018-11-06 14:29:41 -0500

    handlePair() func error patch

--- resources/pushToStrip.php
+++ resources/pushToStrip.php
@@ -9,7 +9,7 @@ $rate;
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_POST as $key => $value){
-        handlePair($key,$value,$colorArray,$brightness);
+        handlePair($key,$value,$colorArray,$brightness,$animation,$rate);
     }
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness.' '.$animation.' '.$rate;
     echo "python exec string: ".$execString;
@@ -21,20 +21,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Handle AJAX cgi bin use case (COMMON)
     foreach($_GET as $key => $value){
         echo "key: ".$key."     value: ".$value;
-        handlePair($key,$value,$colorArray,$brightness);
+        handlePair($key,$value,$colorArray,$brightness,$animation,$rate);
     }
     $execString = 'python3 '.$config['paths']['cgi'].' '.$colorArray.' '.$brightness.' '.$animation.' '.$rate;
     echo "python exec string: ".$execString;
     $str = shell_exec($execString);
     echo $str;
 }
-function handlePair($k,$v,&$src,&$btr){
+function handlePair($k,$v,&$src,&$btr,&$anim,&$rt){
     if($k == 'brightness'){
         $btr = $v;
     } else if ($k == 'animation'){
-        $animation = $v;
+        $anim = $v;
     } else if ($k == 'rate'){
-        $rate = $v;
+        $rt = $v;
     } else {
         $src.=$v;
 
